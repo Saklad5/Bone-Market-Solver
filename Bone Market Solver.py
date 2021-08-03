7f750f8dab6c18d35ea48184b96d0d20b6f403e8
Jeremy Saklad <saklad5@me.com>
2021-08-03 12:31:20 -0500

    Move Declaration to different file

--- Bone Market Solver.py
+++ Bone Market Solver.py
@@ -14,6 +14,7 @@ from ortools.sat.python import cp_model
 from data.adjustments import Adjustment
 from data.appendages import Appendage
 from data.costs import Cost
+from data.declarations import Declaration
 from data.skulls import Skull
 from data.torsos import Torso
 from objects.action import Action
@@ -69,69 +70,6 @@ def AddGeneralMultiplicationEquality(self, target, *variables):
 
 cp_model.CpModel.AddGeneralMultiplicationEquality = AddGeneralMultiplicationEquality
 del AddGeneralMultiplicationEquality
-
-
-class Declaration(Enum):
-    """An action that is taken after all adjustments have been made to a skeleton."""
-
-    AMPHIBIAN = Action(
-            "Declare your (Skeleton Type) a completed Amphibian",
-            cost = Cost.ACTION.value
-            )
-
-    APE = Action(
-            "Declare your (Skeleton Type) a completed Ape",
-            cost = Cost.ACTION.value
-            )
-
-    BIRD = Action(
-            "Declare your (Skeleton Type) a completed Bird",
-            cost = Cost.ACTION.value
-            )
-
-    CHIMERA = Action(
-            "Declare your (Skeleton Type) a completed Chimera",
-            cost = Cost.ACTION.value,
-            implausibility = 3
-            )
-
-    CURATOR = Action(
-            "Declare your (Skeleton Type) a completed Curator",
-            cost = Cost.ACTION.value
-            )
-
-    FISH = Action(
-            "Declare your (Skeleton Type) a completed Fish",
-            cost = Cost.ACTION.value
-            )
-
-    HUMANOID = Action(
-            "Declare your (Skeleton Type) a completed Humanoid",
-            cost = Cost.ACTION.value
-            )
-
-    INSECT = Action(
-            "Declare your (Skeleton Type) a completed Insect",
-            cost = Cost.ACTION.value
-            )
-
-    MONKEY = Action(
-            "Declare your (Skeleton Type) a completed Monkey",
-            cost = Cost.ACTION.value
-            )
-
-    REPTILE = Action(
-            "Declare your (Skeleton Type) a completed Reptile",
-            cost = Cost.ACTION.value
-            )
-
-    SPIDER = Action(
-            "Declare your (Skeleton Type) a completed Spider",
-            cost = Cost.ACTION.value
-            )
-
-    def __str__(self):
-        return str(self.value)
 
 
 class Embellishment(Enum):
